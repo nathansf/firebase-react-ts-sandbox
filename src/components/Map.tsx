f10cc510f50efb940e1b2c28832a01dfc50ecea7
Nathan Frankel <frankel.nathan@gmail.com>
2025-09-13 17:45:28 -0700

    attempt to fix map icon on prod

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,6 +4,21 @@ import type { Map as LeafletMap } from "leaflet";
 import { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Explicitly tell TS we're working with an index signature
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: () => string })
+  ._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 interface MapProps {
   place: Place | null;
 }
