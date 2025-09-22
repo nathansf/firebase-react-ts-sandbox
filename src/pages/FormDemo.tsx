8ee8b82afce66a2e9486e7ac7af52a10f47b8648
Nathan Frankel <frankel.nathan@gmail.com>
2025-09-22 14:48:16 -0700

    Embed form cade

--- src/pages/FormDemo.tsx
+++ src/pages/FormDemo.tsx
@@ -1,17 +1,27 @@
 import Form from "../components/Form";
+import GitHubEmbed from "../utils/GitHubEmbed";
 
 export default function FormDemo() {
   return (
-    <div className="max-w-md mx-auto mt-8">
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">
-        From example with a mix of controlled and uncontrolled inputs
-      </h2>
-      <p>
-        Phone number is the only controlled field, since it requires formatting.
-        Using uncontrolled inputs for other inputs avoids unnecessary renders
-        and simplifies code by using built in form validation.
-      </p>
-      <Form />
-    </div>
+    <>
+      <div className="max-w-md mx-auto mt-8">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">
+          From example with a mix of controlled and uncontrolled inputs
+        </h2>
+        <ul className="list-disc list-inside text-gray-700">
+          <li className="mb-2">
+            Phone number is the only controlled field, since it requires
+            formatting. Using uncontrolled inputs for other inputs and a
+            separate component for phone input avoids unnecessary renders and
+            simplifies code by using built in form validation.
+          </li>
+          <li className="mb-2">Use FormData for form data</li>
+        </ul>
+
+        <Form />
+      </div>
+      <GitHubEmbed url="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2Fnathansf%2Ffirebase-react-ts-sandbox%2Fblob%2Fmain%2Fsrc%2Fcomponents%2FForm.tsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFileMeta=on&showFullPath=on&showCopy=on" />
+      <GitHubEmbed url="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2Fnathansf%2Ffirebase-react-ts-sandbox%2Fblob%2Fmain%2Fsrc%2Fcomponents%2FPhoneInput.tsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFileMeta=on&showFullPath=on&showCopy=on" />
+    </>
   );
 }
