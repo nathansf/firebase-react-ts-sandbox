d63ffcfa74fb0ed2737f53886a339f867f1fce0a
Nathan Frankel <frankel.nathan@gmail.com>
2025-09-26 15:03:36 -0700

    update title of code embed

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -116,7 +116,7 @@ export default function Todo() {
       </div>
       <GitHubEmbed
         url="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2Fnathansf%2Ffirebase-react-ts-sandbox%2Fblob%2Fmain%2Fsrc%2Fpages%2FTodo.tsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFileMeta=on&showFullPath=on&showCopy=on"
-        title="Example usage of debounce hook"
+        title="Code for Todo list example and passing set state callback as prop in typescript"
       />
     </>
   );
